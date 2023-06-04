8d30ab0b2df02471fcbc051c9a92ee1b0cf3f057
amalsetten <124343610+amalsetten@users.noreply.github.com>
2023-06-04 04:20:53 +0000

    Auto detect dark and light mode

---
+++ script.js
@@ -0,0 +1,38 @@
+(function() {
+  try {
+    function updateTheme() {
+      var d = document.documentElement,
+        c = d.classList;
+      c.remove('light', 'dark');
+      var e = localStorage.getItem('theme');
+      if ('system' === e || (!e && true)) {
+        var t = '(prefers-color-scheme: dark)',
+          m = window.matchMedia(t);
+        if (m.media !== t || m.matches) {
+          d.style.colorScheme = 'dark';
+          c.add('dark')
+        } else {
+          d.style.colorScheme = 'light';
+          c.add('light')
+        }
+      } else if (e) {
+        c.add(e || '')
+      }
+      if (e === 'light' || e === 'dark') d.style.colorScheme = e
+    }
+
+    // Update theme on page load
+    updateTheme();
+
+    // Update theme when color scheme preference changes
+    window.matchMedia('(prefers-color-scheme: dark)').addListener(() => {
+      updateTheme();
+    });
+
+    window.matchMedia('(prefers-color-scheme: light)').addListener(() => {
+      updateTheme();
+    });
+
+  } catch (e) { }
+})()
+
