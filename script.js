bb440cc1b99cb17506074c416185ccbce573e0c9
amalsetten <124343610+amalsetten@users.noreply.github.com>
2023-06-11 13:14:20 +0000

    Improve

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 (function() {
   try {
     function updateTheme() {
-      var d = document.documentElement,
-        c = d.classList;
+      const d = document.documentElement;
+      const c = d.classList;
       c.remove('light', 'dark');
-      var e = localStorage.getItem('theme');
-      if ('system' === e || (!e && true)) {
-        var t = '(prefers-color-scheme: dark)',
+      const e = localStorage.getItem('theme');
+      if (e === 'system' || (!e && true)) {
+        const t = '(prefers-color-scheme: dark)',
           m = window.matchMedia(t);
-        if (m.media !== t || m.matches) {
+        if (m.matches) {
           d.style.colorScheme = 'dark';
-          c.add('dark')
+          c.add('dark');
         } else {
           d.style.colorScheme = 'light';
-          c.add('light')
+          c.add('light');
         }
       } else if (e) {
-        c.add(e || '')
+        c.add(e || '');
       }
-      if (e === 'light' || e === 'dark') d.style.colorScheme = e
+      if (e === 'light' || e === 'dark') d.style.colorScheme = e;
     }
 
     // Update theme on page load
     updateTheme();
 
     // Update theme when color scheme preference changes
-    window.matchMedia('(prefers-color-scheme: dark)').addListener(() => {
-      updateTheme();
-    });
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
 
-    window.matchMedia('(prefers-color-scheme: light)').addListener(() => {
-      updateTheme();
-    });
+    window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', updateTheme);
 
   } catch (e) { }
-})()
-
+})();
