3b0a4a0f324067c84c739e5ee9ef660026bc4c61
amalsetten <124343610+amalsetten@users.noreply.github.com>
2023-07-01 02:03:27 +0000

    Update

--- script.js
+++ script.js
@@ -3,31 +3,28 @@
     function updateTheme() {
       const d = document.documentElement;
       const c = d.classList;
-      c.remove('light', 'dark');
+      c.remove('light-theme', 'dark-theme');
       const e = localStorage.getItem('theme');
       if (e === 'system' || (!e && true)) {
         const t = '(prefers-color-scheme: dark)',
           m = window.matchMedia(t);
         if (m.matches) {
           d.style.colorScheme = 'dark';
-          c.add('dark');
+          c.add('dark-theme');
         } else {
           d.style.colorScheme = 'light';
-          c.add('light');
+          c.add('light-theme');
         }
       } else if (e) {
-        c.add(e || '');
+        c.add(e + '-theme' || '');
       }
       if (e === 'light' || e === 'dark') d.style.colorScheme = e;
     }
 
-    // Update theme on page load
     updateTheme();
 
-    // Update theme when color scheme preference changes
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme);
 
     window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', updateTheme);
-
   } catch (e) { }
 })();
